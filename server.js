5a1c06dfeeb549c36a8074f107d2711c241dd154
bcat1023 <bthecat984@gmail.com>
2021-05-13 15:01:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 var http = require('http');
-var https = require('https');
-var httpProxy = require('http-proxy');
 var url = require('url');
-var fs = require('fs');
-const isIPv4 = require('net');
-var cp = require("child_process");
-const version = require('os');
-var proxy = httpProxy.createProxyServer({});
-https.createServer(function(req, res) {
-    var q = url.parse(req.url, true);
-  var filename = q.pathname;
-  var reqip = req.connection.remoteAddress;
-    if (404) {
-        res.writeHead(404);
-        res.write(`ERROR, 404, No webpage was found for this URL. Make sure its correct (at the moment https is not supported, try http://{url here}.com)`)
-    }
-    proxy.web(req, res, { target:`https://github.com`});
-}).listen(3000);
+var request = require('request');
 
-http.createServer(function(req, res) {
-    var q = url.parse(req.url, true);
-  var filename = q.pathname;
-  var reqip = req.connection.remoteAddress;
-    if (404) {
-        res.writeHead(404);
-        res.write(`ERROR, 404, No webpage was found for this URL. Make sure its correct (at the moment https is not supported, try http://{url here}.com)`)
+http.createServer(onRequest).listen(3000);
+
+function onRequest(req, res) {
+
+    var queryData = url.parse(req.url, true).query;
+    if (queryData.url) {
+        request({
+            url: queryData.url
+        }).on('error', function(e) {
+            res.end(e);
+        }).pipe(res);
     }
-    proxy.web(req, res, { target:`http://github.com`});
-}).listen(3001);
+    else {
+        res.end("no url found");
+    }
+}
