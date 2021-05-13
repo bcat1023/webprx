6abc5402865a84fd007408e795a9afc6ff9695dd
bcat1023 <bthecat984@gmail.com>
2021-05-13 15:35:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+console.log(`Application Started`);
 var http = require('http');
 var url = require('url');
 var request = require('request');
+http.createServer(onRequest).listen(3000, `10.0.0.158`);
+console.log(`Http Server Online`);
+console.log(`Listing On localhost:3000`)
+function onRequest(req, res) {
 
-http.createServer(onRequest).listen(3000);
+    var queryData = url.parse(req.url, true).query;
+    if (queryData.url) {
+        request({
+            url: queryData.url
+        }).on('error', function(e) {
+            res.end(e);
+        }).pipe(res);
+    }
+    else {
+        res.end("No url found, 404, Check the url.");
+    }
+}
 
+http.createServer(onRequest).listen(3001, `loguchat.herokuapp.com`);
+console.log(`Http Server Online`);
+console.log(`Listing On localhost:3000`)
 function onRequest(req, res) {
 
     var queryData = url.parse(req.url, true).query;
