2c20d8a4d13c65c307dc4faafa82984362a46f72
Brandon Gillen <gillen@Brandons-MacBook-Pro.local>
2023-09-14 09:17:01 -0700

    Added new game

--- js/primary.js
+++ js/primary.js
@@ -93,7 +93,7 @@ function tick1() {
 
 function tick2() {
   setTimeout(() => {
-    document.getElementById('tick').innerText = "Node Prox is temporarily unavailable until further notice"
+    document.getElementById('tick').innerText = "Flash Makes a return to Titan Network"
     document.getElementById('tick').style.opacity = 1
     setTimeout(() => {
       document.getElementById('tick').style.opacity = 0
@@ -104,7 +104,7 @@ function tick2() {
 
 function tick3() {
   setTimeout(() => {
-    document.getElementById('tick').innerText = "Donkey Kong will be released soon"
+    document.getElementById('tick').innerText = "Out Now: Bloons TD 5"
     document.getElementById('tick').style.opacity = 1
     setTimeout(() => {
       document.getElementById('tick').style.opacity = 0
